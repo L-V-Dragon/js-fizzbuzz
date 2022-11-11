d59f94a2706760f7cadfa92661d7b8916a786af5
L-V-Dragon <viotti37@gmail.com>
2022-11-11 15:33:08 +0100

    Es completo

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ let containerCriminale = document.querySelector(".containerCrim");
 
 for (let i = 1; i < 101; i++){
 
-    if(i % 3 === 0){
+    if(i % 3 == 0 && i % 5 == 0){
 
-        document.getElementById(".containerCrim").innerHTML = '<div class="square"><p>Fizz</p></div>';
+        containerCriminale.innerHTML += '<div class="square"><p>FizzBuzz</p></div>';
 
-    } else if(i % 5 === 0){
+    } else if(i % 3 == 0){
 
-        document.getElementById(".containerCrim").innerHTML = '<div class="square"><p>Buzz</p></div>';
+        containerCriminale.innerHTML += '<div class="square"><p>Fizz</p></div>';
 
-    } else if(i % 3 === 0 && i % 5 === 0){
+    } else if(i % 5 == 0){
 
-        document.getElementById(".containerCrim").innerHTML = '<div class="square"><p>FizzBuzz</p></div>';
+        containerCriminale.innerHTML += '<div class="square"><p>Buzz</p></div>';
 
+    } else {
+        containerCriminale.innerHTML += '<div class="square">'+i+'</div>';
     }
-    
+
 }
